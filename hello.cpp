5edb5f1ca93929ee65a2da53ecbe3ff15e7d5b4d
uditreddys <uditsreddy@gmail.com>
2025-03-13 16:21:42 +0530

    Testing Webhook Integration

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,6 @@
-//File: hello.cpp
 #include <iostream>
 using namespace std;
-
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
-  return 0;
+int main() {
+    cout << "Hello, Jenkins with Webhook!" << endl;
+    return 0;
 }
